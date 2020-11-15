419f9a30d32ddcc15f3360b19c3a1395399bcae2
dev.darshana991 <dev.darshana92@gmail.com>
2020-11-15 09:39:24 +0530

    init

--- src/app/project/poc-form/poc-form.component.ts
+++ src/app/project/poc-form/poc-form.component.ts
@@ -11,5 +11,6 @@ export class PocFormComponent implements OnInit {
 
   ngOnInit() {
   }
+ 
 
 }
--- src/app/project/poc-form/poc-form.module.ts
+++ src/app/project/poc-form/poc-form.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PocFormComponent } from './poc-form.component';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon'
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 @NgModule({
   declarations: [ PocFormComponent ],
   imports: [
     CommonModule,
-    MatCardModule
-    
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class PocFormModule { }
